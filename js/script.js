5213df266fdc02ac1ab0fe9c90a905e29cdab19a
मयंक <126972040+MayankDevil@users.noreply.github.com>
2024-02-01 13:01:44 +0000

    Add js/script

---
+++ js/script.js
@@ -0,0 +1,45 @@
+/*
+-   Project-2 "GUPT"
+-   Designed | Developed by Mayank & HRitik
+-   Copyright by Mayank( https://github.com/MayankDevil/ ) & HRitik ( https://github.com/Hritikkumar975/ )
+-   JavaScript : ./js/script.js
+*/
+try
+{
+    // todocode logic application here
+
+    /*
+        --------------------------------------------------------------------------------
+        } random function : return random number between pass arugment range
+        --------------------------------------------------------------------------------
+    */
+
+    function random(range)
+    {
+        return Math.floor(Math.random() * range)
+    }
+
+    // console.log(random(10))
+    
+    /*
+        --------------------------------------------------------------------------------
+        } getRandom function : return random_index_data by pass argument data object
+        --------------------------------------------------------------------------------
+    */
+    
+    function getRandom(data)
+    {
+        return data[random(data.length)]
+    }
+
+    
+
+
+
+    document.title = `Mayank & HRitik`
+}
+catch(error)
+{
+    console.error(` ERROR ${ error }`)
+}
+// the end
