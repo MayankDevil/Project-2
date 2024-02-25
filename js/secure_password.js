f2204d3e5bb3e98c27510db1237a1e876f573380
मयंक <mastermayank91@gmail.com>
2024-02-25 13:47:45 +0000

    May be update js/ ?

--- js/secure_password.js
+++ js/secure_password.js
@@ -38,65 +38,65 @@ try
         }
         else
         {
-            // if (/[A-Z]/.test(secure_password_fld.value))
-            // {
-            //     secure += 20
-            // }
-            // else
-            // {
-            //     alert_statement += `<div> Capital Letter in this (A-Z) </div>`
-            // }
-            // if (/[a-z]/.test(secure_password_fld.value))
-            // {
-            //     secure += 20
-            // }
-            // else
-            // {
-            //     alert_statement += `<div> Small Letter in this (a-z) </div>`
-            // }
-            // if (/\d/.test(secure_password_fld.value))
-            // {
-            //     secure += 20
-            // }
-            // else
-            // {
-            //     alert_statement += `<div> Number in this (0-9) </div>`
-            // }
-            // if (/[!@#$%^&*(),.?":{}|<>]/.test(secure_password_fld.value))
-            // {
-            //     secure += 20
-            // }
-            // else
-            // {
-            //     alert_statement += `<div> Symbol in this (! @ # $ % &) </div>`
-            // }
-            // if ()
-            // {
-            //     secure += 20
-            // }
+            if (/[A-Z]/.test(secure_password_fld.value))
+            {
+                secure += 20
+            }
+            else
+            {
+                alert_statement += `<div> Capital Letter in this (A-Z) </div>`
+            }
+            if (/[a-z]/.test(secure_password_fld.value))
+            {
+                secure += 20
+            }
+            else
+            {
+                alert_statement += `<div> Small Letter in this (a-z) </div>`
+            }
+            if (/\d/.test(secure_password_fld.value))
+            {
+                secure += 20
+            }
+            else
+            {
+                alert_statement += `<div> Number in this (0-9) </div>`
+            }
+            if (/[!@#$%^&*(),.?":{}|<>]/.test(secure_password_fld.value))
+            {
+                secure += 20
+            }
+            else
+            {
+                alert_statement += `<div> Symbol in this (! @ # $ % &) </div>`
+            }
+            if (true)
+            {
+                secure += 20
+            }
 
-            // switch(parseInt(secure))
-            // {
-            //     case 100:
-            //         alert_statement += `<div class='isValid'> Very Good Password <div>`
-            //         break;
-            //     case 80 :
-            //         alert_statement += `<div class='isValid'> Your password Security Level is very High <div>`
-            //         break;
-            //     case 60 :
-            //         alert_statement += `<div class='isValid'> Your password Level is Secure, Nice! <div>`
-            //         break;
-            //     case 40 :
-            //         alert_statement += `<div class='isValid'> Password is Weak <div>`
-            //         break;    
-            //     case 20 :
-            //         alert_statement += `<div class='isUnvalid'> Require better password <div>`
-            //         break;
-            //     default :
-            //         alert_statement += `<div class='isUnvalid'> Your security in Danger <div>` 
-            // }
+            switch(parseInt(secure))
+            {
+                case 100:
+                    alert_statement += `<div class='isValid'> Very Good Password <div>`
+                    break;
+                case 80 :
+                    alert_statement += `<div class='isValid'> Your password Security Level is very High <div>`
+                    break;
+                case 60 :
+                    alert_statement += `<div class='isValid'> Your password Level is Secure, Nice! <div>`
+                    break;
+                case 40 :
+                    alert_statement += `<div class='isValid'> Password is Weak <div>`
+                    break;    
+                case 20 :
+                    alert_statement += `<div class='isUnvalid'> Require better password <div>`
+                    break;
+                default :
+                    alert_statement += `<div class='isUnvalid'> Your security in Danger <div>` 
+            }
 
-            // secure_alert.innerHTML = `<div> Your Password is ${secure}% secure <div> ${alert_statement} `
+            secure_alert.innerHTML = `<div> Your Password is ${secure}% secure <div> ${alert_statement} `
 
             console.log((secure_password_fld.value.match(/[a-zA-Z]/g) || []).length + secure_password_fld.value.length)
         }
